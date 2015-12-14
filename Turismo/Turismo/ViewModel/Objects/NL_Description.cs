787e7b1499202a2c1294e59625c24f7a548db411
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-12-14 16:36:49 +0100

    ineen voegen van de talen

--- Turismo/Turismo/ViewModel/Objects/NL_Description.cs
+++ Turismo/Turismo/ViewModel/Objects/NL_Description.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Turismo.ViewModel.Components;
 
 namespace Turismo.ViewModel.Objects
 {
-    class NL_Description
+    class NL_Description : Description
     {
+
+    }
+
+    class EN_Description
+    {
+
     }
 }
