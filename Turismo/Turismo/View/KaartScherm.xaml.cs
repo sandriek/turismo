56f2f3f3f4f5b480f772c3f2e7eed0278f5c334e
Remco <Remco.boys@gmail.com>
2015-12-14 14:02:19 +0100

    taalscherm toegevoegd!

--- Turismo/Turismo/View/KaartScherm.xaml.cs
+++ Turismo/Turismo/View/KaartScherm.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Devices.Geolocation;
+using Turismo.View;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -45,7 +46,10 @@ namespace Turismo
             MapControl1.ZoomLevel = 12;
             MapControl1.LandmarksVisible = true;
         }
-
-
+        
+        private void TaalKnop_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(TaalScherm));
+        }
     }
 }
--- Turismo/Turismo/View/TaalScherm.xaml.cs
+++ Turismo/Turismo/View/TaalScherm.xaml.cs
@@ -13,21 +13,21 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Turismo
+namespace Turismo.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class TaalScherm : Page
     {
-        public MainPage()
+        public TaalScherm()
         {
-            this.InitializeComponent();            
+            this.InitializeComponent();
         }
 
-        private void KaartKnop_Click(object sender, RoutedEventArgs e)
+        private void TaalGeselecteerd(object sender, TappedRoutedEventArgs e)
         {
             Frame.Navigate(typeof(KaartScherm));
         }
