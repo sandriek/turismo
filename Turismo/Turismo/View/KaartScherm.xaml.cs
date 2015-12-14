481b42a3f4927365e1808c1ed9eab56cc2aec9a7
Marnix den Baars <m-denbaars1@hotmail.com>
2015-12-14 14:28:10 +0100

    Getmylocation added + key added

--- Turismo/Turismo/View/KaartScherm.xaml.cs
+++ Turismo/Turismo/View/KaartScherm.xaml.cs
@@ -15,6 +15,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Devices.Geolocation;
 using Turismo.View;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -31,10 +32,12 @@ namespace Turismo
             MapControl MapControl2 = new MapControl();
             MapControl2.ZoomInteractionMode = MapInteractionMode.GestureAndControl;
             MapControl2.TiltInteractionMode = MapInteractionMode.GestureAndControl;
-            MapControl2.MapServiceToken = "EnterYourAuthenticationKeyHere";
+            MapControl2.MapServiceToken = "TWf70pkqTqFQpZHbZ2B9~EwuJrmaVK6VR5Y - GOT1AWw~AjSoU2Dy13BDTB2xpdlh5R5CbOBDSRVDxm19MVc9pwkNx5MMXhOJizCTsdfbbLvz";
             pageGrid.Children.Add(MapControl2);
+            GetMyLocation();
         }
 
+        // Voorbeeld voor het setten van een map location met coördinaten
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             // Specify a known location.
@@ -46,10 +49,44 @@ namespace Turismo
             MapControl1.ZoomLevel = 12;
             MapControl1.LandmarksVisible = true;
         }
-        
+
+        public async void GetMyLocation()
+        {
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            switch (accessStatus)
+            {
+                case GeolocationAccessStatus.Allowed:
+                    Debug.WriteLine("works");
+                    // Get the current location.
+                    Geolocator geolocator = new Geolocator();
+                    Geoposition pos = await geolocator.GetGeopositionAsync();
+                    Geopoint myLocation = pos.Coordinate.Point;
+
+                    // Set the map location.
+                    MapControl1.Center = myLocation;
+                    MapControl1.ZoomLevel = 12;
+                    MapControl1.LandmarksVisible = true;
+                    break;
+
+                case GeolocationAccessStatus.Denied:
+                    // Handle the case  if access to location is denied.
+
+                    Debug.WriteLine("Denied");
+                    break;
+
+                case GeolocationAccessStatus.Unspecified:
+                    // Handle the case if  an unspecified error occurs.
+
+                    Debug.WriteLine("Not specified");
+                    break;
+            }
+
+        }
+
         private void TaalKnop_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(TaalScherm));
         }
+
     }
 }
