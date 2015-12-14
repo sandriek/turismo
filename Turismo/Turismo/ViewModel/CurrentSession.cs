ebf03d03ab17884eae105dd088a5bfcb67cfe986
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-12-14 15:38:36 +0100

    currentsession bestaat

---
+++ Turismo/Turismo/ViewModel/CurrentSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Turismo.ViewModel
+{
+    class CurrentSession
+    {
+        private GeoPosition CurrentLocation;
+        private List<Location> FollowedRoute;
+        private Route CurrentRoute;
+        private Language Currentlanguage;
+
+        public CurrentSession()
+        {
+
+        }
+
+        public void SwitchLanguage()
+        {
+
+        }
+    }
+}
