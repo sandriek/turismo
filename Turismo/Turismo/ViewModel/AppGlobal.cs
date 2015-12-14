553fb00244c368663311e7a22d8611e79516dba3
Jeroentjuh99 <jeroenvanos2@gmail.com>
2015-12-14 14:27:30 +0100

    een troeplading errors XD

---
+++ Turismo/Turismo/ViewModel/AppGlobal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Turismo.ViewModel
+{
+    public class AppGlobal
+    {
+        private AppGlobal _instance;
+        private GeoUtil _GeoUtil;
+        private List<Site> SiteList;
+        private List<Route> RouteList;
+        private CurrentSession _CurrentSession;
+
+    }
+}
