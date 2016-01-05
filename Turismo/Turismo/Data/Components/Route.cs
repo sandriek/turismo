a68d34d05a27199b7cdc08fa0c10faa355a0aaf9
Dofensmirtsz <dofensmirtsz@gmail.com>
2016-01-05 20:46:24 +0100

    Route er in zetten gemaakt

--- Turismo/Turismo/Data/Components/Route.cs
+++ Turismo/Turismo/Data/Components/Route.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +13,72 @@ namespace Turismo.Components
     {
         string Name;
         List<Location> LocationList;
-        Description Description;
-        Category category;
+        Description _Description;
+        Category _Category;
+
+
+        double NorthLatitude;
+        double WesternLongitude;
+        string NameSite;
+        Boolean IsItASite = false;
+
+        public Route(string name, Description description, Category category)
+        {
+            this.Name = name;
+            this._Description = description;
+            this._Category = category;
+            LocationList = new List<Location>();
+            FillLocationList();
+        }
+
+        private void FillLocationList()
+        {
+            string filename = "Assets/" + Name + ".txt";
+            if (File.Exists(filename))
+            {
+                string[] route = File.ReadAllLines(filename);
+                foreach(string s in route)
+                {
+                    string[] delen = s.Split(',');
+
+                    try
+                    {
+                        NorthLatitude = Convert.ToDouble(delen[0]);
+                    }
+                    catch (FormatException e)
+                    {
+                        Debug.WriteLine(e.Message);
+                    }
+
+
+
+                    try
+                    {
+                        WesternLongitude = Convert.ToDouble(delen[1]);
+                    }
+                    catch (FormatException e)
+                    {
+                        Debug.WriteLine(e.Message);
+                    }
+
+                    NameSite = delen[2];
+
+                    if (delen[2] != " ")
+                        IsItASite = true;
+                        
+
+
+
+                    Location RP = new RoutePoint(NameSite, _Description); //hier moeten dan nog die NB en WL komen
+                    LocationList.Add(RP);
+                }
+            }
+            else
+            {
+                Debug.WriteLine("Het gewenste bestand is niet gevonden.");
+            }
+            
+
+        }
     }
 }
--- Turismo/Turismo/Data/Components/RoutePoint.cs
+++ Turismo/Turismo/Data/Components/RoutePoint.cs
@@ -9,12 +9,20 @@ using Windows.Devices.Geolocation.Geofencing;
 
 namespace Turismo.Components
 {
-    class RoutePoint
+    class RoutePoint : Location
     {
         public string name { get; set; }
         public Geoposition Position { get; set; }
         public Geofence Fence { get; set; }
         public List<string> ImageGallery { get; set; }
         public Description Description { get; set; }
+
+        //kan ik hier niet beter gewoon doubles door geven ivm noorderbreedte en westerlengte?
+
+        public RoutePoint(string name, Description Description)
+        {
+            this.name = name;
+            this.Description = Description;
+        }
     }
 }
