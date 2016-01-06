6b16e7613bfdc96347ec5149226737e2bef12345
Remco <Remco.boys@gmail.com>
2016-01-06 11:36:46 +0100

    can switch between languages with type of class MutipleLanguageString

--- Turismo/Turismo/Pages/TaalScherm.xaml.cs
+++ Turismo/Turismo/Pages/TaalScherm.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Turismo.ViewModels;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +26,7 @@ namespace Turismo.Pages
         public TaalScherm()
         {
             this.InitializeComponent();
+            DataContext = TaalSchermViewModel.Instance;
         }
 
         private void TaalGeselecteerd(object sender, TappedRoutedEventArgs e)
---
+++ Turismo/Turismo/ViewModels/TaalSchermViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Turismo.Data;
+using Turismo.Data.Objects;
+
+namespace Turismo.ViewModels
+{
+    public class TaalSchermViewModel
+    {
+        private static TaalSchermViewModel _instance;
+        public static TaalSchermViewModel Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new TaalSchermViewModel();
+                }
+                return _instance;
+            }
+        }
+
+
+        private TaalSchermViewModel()
+        {
+            Title = new MutipleLanguageString("Kies een taal", "Choose a language");
+            CurrentSession.LanguageChanged += CurrentSession_LanguageChanged;
+        }
+
+        private void CurrentSession_LanguageChanged(object sender, EventArgs e)
+        {
+            Title.SetText();
+        }
+
+        public MutipleLanguageString Title { get; }
+    }
+}
