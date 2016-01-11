58333ff3eb61ab70d73acf8825dbd98e1709b3cc
Jeroentjuh99 <jeroenvanos2@gmail.com>
2016-01-11 15:29:10 +0100

    info pagina geupdate

--- Turismo/ViewModels/InfoSchermViewModel.cs
+++ Turismo/ViewModels/InfoSchermViewModel.cs
@@ -23,7 +23,7 @@ namespace Turismo.ViewModels
             SiteLabel = new MultipleLanguageString("Een bezienswaardigheid", "A Sight to visit");
             TaalSelectLabel = new MultipleLanguageString("Door op deze knop te drukken kan je een andere taal selecteren", "To change the language press this button");
             RouteLabel = new MultipleLanguageString("Hier kan je een route selecteren", "Click this button to choose a route");
-            PauseLabel = new MultipleLanguageString("Met deze knop pauzeer je het lopen van een route. Dit bespaart Batterij", "This option will pause the map and will save battery life");
+            PauseLabel = new MultipleLanguageString("Met deze knop pauzeer je het lopen van een route. Dit bespaart batterijspanning. \n Als de applicatie op pauze staat kan je vrij door de map scrollen", "This option will pause the map and will save battery life. \n When the app is paused, you can scroll freely through the map.");
         }
     }
 }
